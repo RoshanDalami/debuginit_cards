648e69bde80b5479e5ecfc0a0a04c1be7cc950cd
rsunar <tech.channel.002@gmail.com>
2022-09-23 20:55:37 +0545

    starting day

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card01 from './Components/Cards/Card01/Card01';
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <Card01 />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Cards/Card01/Card01.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from './Card01.module.css';
+const Card01 =()=>{
+    return(
+        <div className={styles.card}></div>
+    );
+};
+
+export default Card01 ;
