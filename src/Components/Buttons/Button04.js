465c947d2d10444ff8bb6c9a818c85b43d0f1598
rsunar <tech.channel.002@gmail.com>
2022-09-26 08:01:03 +0545

    starting card 4

---
+++ src/Components/Buttons/Button04.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Button04.module.css'
+function Button04() {
+  return (
+    <div className={styles.button}>
+    <button type='submit' className={styles.btn4} >ADD TO CART</button>
+</div>
+  )
+}
+
+export default Button04
